0fb752d63cd3472c6b64607502c2e4f5adf945f4
FABIEN GLORY <fabien.glory@decathlon.com>
2018-01-30 18:15:17 +0100

    total added

--- shopshopreact/src/modules/cart/cart.js
+++ shopshopreact/src/modules/cart/cart.js
@@ -14,18 +14,24 @@ class Cart extends React.Component{
     <tr key="product.id">
     <td>{product.decathlon_id}</td>
     <td>{product.title}</td>
-    <td>{product.min_price}€</td>
     <td><img alt={product.title} src={"https://www.decathlon.fr/media/"+product.image_path}/></td>
+    <td>{product.min_price} €</td>
     <td><button onClick={this.props.decrement} value = {index} >-</button></td>
     <td>{product.quantity}</td>
     <td><button onClick={this.props.increment} value = {index} >+</button></td>
     <td><button onClick={this.props.remove} value = {index} >Remove</button></td>
+    <td>{product.quantity*product.min_price} €</td>
   </tr>)
+    let sumPrice=0
+    const totalPrice= this.props.productsCarted.map((product) =>
+     sumPrice += product.quantity*product.min_price
+  )
 
     return(
       <div>
         <h1>Cart</h1>
         <table><tbody>{cartDisplay}</tbody></table>
+        <h1>The total price is {sumPrice} €</h1>
       </div>
     )
   }
