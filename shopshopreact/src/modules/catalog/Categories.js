7a28c5f0dde2ca4e3c88f43812c27890284c050d
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2018-01-30 17:54:38 +0100

    src/modules/catalog/Categories.js

--- shopshopreact/src/modules/catalog/Categories.js
+++ shopshopreact/src/modules/catalog/Categories.js
@@ -6,24 +6,48 @@ import { getCatalog } from "../../store/catalog/selectors";
 import Products from './Products';
 
 class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: ""
+    };
+  }
+
   componentDidMount(){
     this.props.retrieveCategories();
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      current: event.target.value
+    });
+  };
+
   render() {
-    let listCatDisplay = this.props.categories.map((category) =>
-        <li
+    let listCatReduce = this.props.categories.map((category) =>
+      {
+      if(category.label.includes(this.state.current)){
+        return (<li
           key={category.decathlon_id}
           onClick={() => this.props.getProductsFromCategory(category.id)}>{category.label}
-        </li>
+        </li>)
+      }
+      }
     )
 
     return (
       <div className="container">
         <div className="listCat">
           <h1>Categories</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div class="form-group">
+              <label for="search-category">Search categories</label>
+              <input type="text" class="form-control" id="search-category" value={this.state.current} onChange={this.handleSubmit}/>
+            </div>
+          </form>
           <ul>
-            {listCatDisplay}
+            {listCatReduce}
           </ul>
         </div>
         <Products />
