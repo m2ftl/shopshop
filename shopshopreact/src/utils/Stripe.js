b6572ae00ec9be083f4f6e46ed2e82938431b75b
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-02-01 18:33:06 +0100

    add home btn on order page

--- shopshopreact/src/utils/Stripe.js
+++ shopshopreact/src/utils/Stripe.js
@@ -3,6 +3,8 @@ import "../App.css";
 import StripeCheckout from "react-stripe-checkout";
 import { connect } from "react-redux";
 import { cartActions } from "../store/cart/actions";
+import { Link } from "react-router-dom";
+
 
 class Stripe extends Component {
   onToken = token => {
@@ -27,53 +29,60 @@ class Stripe extends Component {
   };
   render() {
     return (
-        <div class="ml-4">
-        <h1>Proceed my Order</h1>
-        <h2>Step 1 - Fill my delivery address</h2>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="form-group mr-3">
-              <label for="usr">First Name:</label>
-              <input
-                type="text"
-                value={this.props.givenName}
-                class="form-control"
-              />
+        <div>
+          <div className="mb-3 homeButton">
+            <Link to="/">
+              <i class="fa fa-home fa-2x" aria-hidden="true"></i>
+            </Link>
+          </div>
+          <div class="ml-4">
+          <h1>Proceed my Order</h1>
+          <h2>Step 1 - Fill my delivery address</h2>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="form-group mr-3">
+                <label for="usr">First Name:</label>
+                <input
+                  type="text"
+                  value={this.props.givenName}
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pwd">Family Name:</label>
+                <input
+                  type="text"
+                  value={this.props.familyName}
+                  class="form-control"
+                />
+              </div>
             </div>
-            <div class="form-group">
-              <label for="pwd">Family Name:</label>
-              <input
-                type="text"
-                value={this.props.familyName}
-                class="form-control"
-              />
+            <div class="row">
+              <div class="form-group col-xs-3 ml-0 p-0">
+                <label for="pwd">Address:</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="form-group mr-3">
+                <label for="pwd">ZIP Code:</label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label for="pwd">City:</label>
+                <input type="text" class="form-control" />
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="form-group col-xs-3 ml-0 p-0">
-              <label for="pwd">Address:</label>
-              <input type="text" class="form-control" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="form-group mr-3">
-              <label for="pwd">ZIP Code:</label>
-              <input type="text" class="form-control" />
-            </div>
-            <div class="form-group">
-              <label for="pwd">City:</label>
-              <input type="text" class="form-control" />
-            </div>
-          </div>
-        </div>
 
-        <h2>Step 2 - Proceed your payment</h2>
-          <StripeCheckout
-            token={this.onToken}
-            amount={this.props.totalAmount*100}
-            currency="EUR"
-            stripeKey={process.env.REACT_APP_PUBLISHABLE_KEY}
-          />
+          <h2>Step 2 - Proceed your payment</h2>
+            <StripeCheckout
+              token={this.onToken}
+              amount={this.props.totalAmount*100}
+              currency="EUR"
+              stripeKey={process.env.REACT_APP_PUBLISHABLE_KEY}
+            />
+          </div>
         </div>
     );
   }
